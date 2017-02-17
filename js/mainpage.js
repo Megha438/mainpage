0a020f5fa5cce2f4123c349f02466227234ad3bf
Megha Santhosh <meghasanthosh438@gmail.com>
2017-02-17 23:14:15 +0530

    lala

---
+++ js/mainpage.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.carousel').carousel();
+      $('.slider').slider({full_width: true});
+     
+        
+  //  $('.carousel.carousel-slider').carousel({full_width: true});
+    //$('.carousel').carousel('set', 4);
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
